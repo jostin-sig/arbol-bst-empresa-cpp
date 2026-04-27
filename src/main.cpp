4fcfa6945b2f2b81d948eb075da745452a3ff9ad
Jostin <justin05sigcha@gmail.com>
2026-04-26 21:37:45 -0500

    Se implementa busqueda de empleados en BST

--- src/main.cpp
+++ src/main.cpp
@@ -35,6 +35,15 @@ private:
 
         return nodo;
     }
+    Nodo* buscar(Nodo* nodo, int codigo) {
+        if (nodo == nullptr || nodo->dato.codigo == codigo)
+            return nodo;
+
+        if (codigo < nodo->dato.codigo)
+            return buscar(nodo->izquierdo, codigo);
+        else
+            return buscar(nodo->derecho, codigo);
+    }
 
 public:
     ArbolBST() {
@@ -43,10 +52,26 @@ public:
     void insertarEmpleado(Empleado emp) {
         raiz = insertar(raiz, emp);
     }
+    void buscarEmpleado(int codigo) {
+        Nodo* resultado = buscar(raiz, codigo);
+
+        if (resultado != nullptr) {
+            cout << "\nEmpleado encontrado:\n";
+            cout << "Codigo: " << resultado->dato.codigo << endl;
+            cout << "Nombre: " << resultado->dato.nombre << endl;
+            cout << "Cargo : " << resultado->dato.cargo << endl;
+        } else {
+            cout << "\nEmpleado no encontrado.\n";
+        }
+    }
 };
 
 int main() {
     ArbolBST arbol;
-    cout << "Arbol BST creado correctamente." << endl;
+    arbol.insertarEmpleado({50, "Empresa UTA", "Raiz"});
+    arbol.insertarEmpleado({30, "Gerente Ventas", "Gerente"});
+    arbol.insertarEmpleado({70, "Gerente Finanzas", "Gerente"});
+
+    arbol.buscarEmpleado(30);
     return 0;
 }
