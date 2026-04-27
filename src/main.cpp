4db81b744bc3756fe8da18e9204f6782b86071af
Jostin <justin05sigcha@gmail.com>
2026-04-26 21:51:34 -0500

    Se implemento un menu interativo del sistema

--- src/main.cpp
+++ src/main.cpp
@@ -144,17 +144,62 @@ public:
 
 int main() {
     ArbolBST arbol;
-    arbol.insertarEmpleado({50, "Empresa UTA", "Raiz"});
-    arbol.insertarEmpleado({30, "Gerente Ventas", "Gerente"});
-    arbol.insertarEmpleado({70, "Gerente Finanzas", "Gerente"});
-    arbol.insertarEmpleado({20, "Empleado 1", "Hoja"});
-    arbol.insertarEmpleado({40, "Empleado 2", "Hoja"});
-    arbol.insertarEmpleado({60, "Empleado 3", "Hoja"});
-    arbol.insertarEmpleado({80, "Empleado 4", "Hoja"});
+    int opcion;
 
-    arbol.mostrarRaiz();
-    arbol.mostrarInorden();
-    arbol.mostrarAltura();
-    arbol.mostrarNodosHoja();
+    do {
+        cout << "\n===== MENU ARBOL BST EMPRESARIAL =====\n";
+        cout << "1. Insertar empleado\n";
+        cout << "2. Buscar empleado\n";
+        cout << "3. Mostrar raiz\n";
+        cout << "4. Recorrido inorden\n";
+        cout << "5. Recorrido preorden\n";
+        cout << "6. Recorrido postorden\n";
+        cout << "7. Mostrar altura\n";
+        cout << "8. Mostrar hojas\n";
+        cout << "0. Salir\n";
+        cout << "Seleccione una opcion: ";
+        cin >> opcion;
+
+        if (opcion == 1) {
+            Empleado emp;
+            cout << "Codigo: ";
+            cin >> emp.codigo;
+            cin.ignore();
+
+            cout << "Nombre: ";
+            getline(cin, emp.nombre);
+
+            cout << "Cargo: ";
+            getline(cin, emp.cargo);
+
+            arbol.insertarEmpleado(emp);
+        }
+        else if (opcion == 2) {
+            int codigo;
+            cout << "Ingrese codigo a buscar: ";
+            cin >> codigo;
+            arbol.buscarEmpleado(codigo);
+        }
+        else if (opcion == 3) {
+            arbol.mostrarRaiz();
+        }
+        else if (opcion == 4) {
+            arbol.mostrarInorden();
+        }
+        else if (opcion == 5) {
+            arbol.mostrarPreorden();
+        }
+        else if (opcion == 6) {
+            arbol.mostrarPostorden();
+        }
+        else if (opcion == 7) {
+            arbol.mostrarAltura();
+        }
+        else if (opcion == 8) {
+            arbol.mostrarNodosHoja();
+        }
+
+    } while (opcion != 0);
+
     return 0;
 }
